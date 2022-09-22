c7b7fef74008917a2b6a977c286a3b4b0599ca0e
Ngọc Châu <dnchau2310@gmail.com>
2022-09-22 23:41:09 +0700

    Landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Landing from '../components/Landing';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,17 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header></Header>
+
+      <main className='relative h-[200vh] bg-[#E7ECEE]'>
+        <Landing />
+      </main>
+      <section className='relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]'>
+        <div className='space-y-10 py-16'>
+          <h1 className='text-center text-4xl font-medium tracking-wide text-white md:text-5xl'>
+            New Promos
+          </h1>
+        </div>
+      </section>
     </div>
   );
 };
